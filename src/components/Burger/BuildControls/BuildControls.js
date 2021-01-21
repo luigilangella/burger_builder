8aff9138d2ace5cbf3a9ee670ce87ad9a5bdd0da
Luigi <luigi76langella@gmail.com>
2021-01-21 16:13:56 +0000

    Add logic to control buttons to add-remove ingredients and price

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,5 +1,26 @@
 import React from "react";
+import classes from "../BuildControls/BuildControls.module.css";
+import BuildControl from "./BuildControl/BuildControl";
 
-const buildControls = (props) => <div></div>;
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" },
+];
+
+const buildControls = (props) => (
+  <div className={classes.BuildControls}>
+    {controls.map((ctrl) => (
+      <BuildControl
+        key={ctrl.label}
+        label={ctrl.label}
+        added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
+      />
+    ))}
+  </div>
+);
 
 export default buildControls;
